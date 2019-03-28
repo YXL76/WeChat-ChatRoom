fadf5a6068300ee1cf5f44314c1f035e96af3f67
YXL <chenxin.lan.76@gmail.com>
2019-03-28 17:09:54 +0800

    v1.3.1

--- WeChat-ChatRoom/client/pages/index/index.js
+++ WeChat-ChatRoom/client/pages/index/index.js
@@ -1,8 +1,25 @@
 // pages/index/index.js
+
+var app = getApp()
+
 Page({
-  in : function()  {
+  data: {
+    notGet: true
+  },
+
+  in: function () {
     wx.navigateTo({
       url: '/pages/chat/chat'
     });
-  }
+  },
+
+  get: function (e) {
+    let that = this
+    if (e.detail.userInfo) {
+      that.setData({
+        notGet: false
+      })
+      app.globalData.userInfo = e.detail.userInfo
+    }
+  },
 })
