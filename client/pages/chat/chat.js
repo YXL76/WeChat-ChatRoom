5c2b6ed9990e18bc082407387b209e2b9cb44ceb
YXL <chenxin.lan.76@gmail.com>
2020-02-25 11:59:41 +0800

    2.0

--- client/pages/chat/chat.js
+++ client/pages/chat/chat.js
@@ -1,175 +1,160 @@
 // pages/chat/chat.js
 
-Page({
+const app = getApp()
+
+Component({
+
+  options: {
+    pureDataPattern: /^_/
+  },
+
   data: {
-    isHide: false,
+    theme: 'Grey',
+    systemIn: true,
+    systemOut: true,
+    systemNum: true,
+    _anonymous: false,
+    _anonymousName: '匿名用户^_^',
+    message: '',
+    messageList: [],
+    userInfo: {}
   },
 
-  onLoad: function () {
-    let that = this
-    wx.showLoading({
-      title: '正在进入聊天室'
-    })
-    wx.getStorage({
-      key: 'system_in',
-      fail() {
-        wx.setStorage({
-          key: 'system_in',
-          data: true
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'system_out',
-      fail() {
-        wx.setStorage({
-          key: 'system_out',
-          data: true
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'system_num',
-      fail() {
-        wx.setStorage({
-          key: 'system_num',
-          data: true
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'anonymous',
-      fail() {
-        wx.setStorage({
-          key: 'anonymous',
-          data: false
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'anonymousName',
-      fail() {
-        wx.setStorage({
-          key: 'anonymousName',
-          data: '匿名用户^_^'
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'bg_color',
-      fail() {
-        wx.setStorage({
-          key: 'bg_color',
-          data: 'F2F3F5'
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'user_bg_color',
-      fail() {
-        wx.setStorage({
-          key: 'user_bg_color',
-          data: '0188FB'
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'other_bg_color',
-      fail() {
-        wx.setStorage({
-          key: 'other_bg_color',
-          data: 'FFFFFF'
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'user_font_color',
-      fail() {
-        wx.setStorage({
-          key: 'user_font_color',
-          data: 'FFFFFF'
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'other_font_color',
-      fail() {
-        wx.setStorage({
-          key: 'other_font_color',
-          data: '000000'
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'font_size',
-      fail() {
-        wx.setStorage({
-          key: 'font_size',
-          data: '32'
-        })
-      }
-    })
-    wx.getStorage({
-      key: 'chat_history',
-      fail() {
-        let temp = []
-        wx.setStorage({
-          key: 'chat_history',
-          data: temp
-        })
-      }
-    })
-    setTimeout(function () {
-      wx.hideLoading()
-      that.intoChatroon()
-    }, 1200)
-  },
+  lifetimes: {
 
-  onShow: function () {
-    let that = this
-    if (that.data.isHide == true) {
-      wx.showLoading({
-        title: '正在退出聊天室'
+    attached: function () {
+      this.setData({ userInfo: app.globalData.userInfo })
+      this.getData()
+      this.connect()
+      wx.showLoading({ title: '正在进入聊天室', mask: true })
+      setTimeout(function () { wx.hideLoading() }, 2000)
+    },
+
+    detached: function () {
+      let that = this
+      if (!that.data._anonymous) {
+        wx.sendSocketMessage({
+          data: '{ "content": "' + app.globalData.userInfo.nickName + '退出了聊天室' + '", "type": "systemOut", "nickName": "' + '' + '", "avatarUrl": "' + '' + '" }',
+          complete() {
+            wx.closeSocket()
+            wx.onSocketClose(function (res) { console.log('close success') })
+          }
+        })
+      } else {
+        wx.closeSocket()
+        wx.onSocketClose(function (res) { console.log('close success') })
+      }
+      wx.getStorage({
+        key: 'chatHistory',
+        success(res) {
+          let length = res.data.length + that.data.messageList.length
+          wx.setStorage({
+            key: 'chatHistory',
+            data: res.data.concat(that.data.messageList).slice(length > 99 ? length - 99 : 0)
+          })
+        }
       })
-      setTimeout(function () {
-        wx.hideLoading()
-        wx.navigateBack({
-          delta: 1
-        })
-      }, 1600)
     }
   },
 
-  onHide: function () {
-    let that = this
-    that.setData({
-      isHide: true
-    })
+  pageLifetimes: {
+    show: function () {
+      setTimeout(() => {
+        this.getData()
+      }, 800);
+    }
   },
 
-  intoChatroon: function () {
-    wx.getStorage({
-      key: 'page_state',
-      success(res) {
-        if (res.data == true) {
-          wx.navigateTo({
-            url: '/pages/chat/chatLQ/chatLQ'
-          })
-        } else {
-          wx.navigateTo({
-            url: '/pages/chat/chatHQ/chatHQ'
-          })
+  methods: {
+
+    connect: function () {
+      const that = this
+      wx.connectSocket({ url: 'ws://localhost:3000/' })
+      wx.onSocketOpen(function (res) {
+        console.log('open success')
+        wx.onSocketMessage(function (res) {
+          let list = that.data.messageList
+          let newItem = JSON.parse(res.data)
+          list.push(newItem)
+          that.setData({ messageList: list })
+          if (newItem.type === "text") that.toBottom()
+        })
+        if (!that.data._anonymous) {
+          wx.sendSocketMessage({ data: '{ "content": "' + app.globalData.userInfo.nickName + '进入了聊天室' + '", "type": "systemIn", "nickName": "' + '' + '", "avatarUrl": "' + '' + '" }' })
         }
-      },
-      fail() {
-        wx.setStorage({
-          key: 'page_state',
-          data: true
+      })
+      wx.onSocketError(function (res) {
+        wx.showToast({
+          title: '无法连接服务器，请重试',
+          icon: "none",
+          duration: 3000
         })
-        wx.navigateTo({
-          url: '/pages/chat/chatLQ/chatLQ'
-        })
-      }
-    })
-  },
+      })
+    },
+
+    getData: function () {
+      const that = this
+      wx.getStorage({
+        key: 'theme',
+        success(res) { that.setData({ theme: res.data }) }
+      })
+      wx.getStorage({
+        key: 'systemIn',
+        success(res) { that.setData({ systemIn: res.data }) }
+      })
+      wx.getStorage({
+        key: 'systemOut',
+        success(res) { that.setData({ systemOut: res.data }) }
+      })
+      wx.getStorage({
+        key: 'systemNum',
+        success(res) { that.setData({ systemNum: res.data }) }
+      })
+      wx.getStorage({
+        key: 'anonymous',
+        success(res) { that.setData({ _anonymous: res.data }) }
+      })
+      wx.getStorage({
+        key: 'anonymousName',
+        success(res) { that.setData({ _anonymousName: res.data, }) }
+      })
+    },
+
+    inputMessage: function (e) {
+      this.setData({ message: e.detail.value })
+    },
+
+    sendMessage: function () {
+      if (this.data.message === '') return
+      const that = this
+      const mes = that.data.message
+      that.setData({ message: '' })
+      let temp = ''
+      if (that.data._anonymous) temp = '{ "content": "' + mes + '", "type": "text", "nickName": "[匿名]' + that.data._anonymousName + '", "avatarUrl": "' + app.globalData.userInfo.avatarUrl + '" }'
+      else temp = '{ "content": "' + mes + '", "type": "text", "nickName": "' + app.globalData.userInfo.nickName + '", "avatarUrl": "' + app.globalData.userInfo.avatarUrl + '" }'
+      wx.sendSocketMessage({
+        data: temp,
+        success: function (res) { that.toBottom() },
+        fail: function (res) {
+          wx.showToast({
+            title: '消息发送失败，请重试',
+            icon: "none",
+            duration: 2000
+          })
+        },
+      })
+    },
+
+    toBottom: function () {
+      wx.pageScrollTo({ scrollTop: 100000, duration: 300 })
+    },
+
+    toSetting: function () {
+      wx.navigateTo({ url: '/pages/chat/setting/setting' })
+    },
+
+    toHistory: function () {
+      wx.navigateTo({ url: '/pages/chat/history/history' })
+    }
+  }
 })
---
+++ client/pages/chat/history/history.js
@@ -0,0 +1,34 @@
+// pages/chat/history/history.js
+
+const app = getApp()
+
+Component({
+
+  data: {
+    theme: '',
+    messageList: [],
+    userInfo: {}
+  },
+
+  lifetimes: {
+
+    attached: function () {
+      const that = this
+      that.setData({ userInfo: app.globalData.userInfo })
+      wx.getStorage({
+        key: "chatHistory",
+        success(res) {
+          that.setData({ messageList: res.data })
+        }
+      })
+      wx.getStorage({
+        key: "theme",
+        success(res) { that.setData({ theme: res.data }) }
+      })
+    },
+
+    ready: function () {
+      wx.pageScrollTo({ scrollTop: 100000, duration: 300 })
+    }
+  }
+})
